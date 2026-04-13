0287b79aba5477cc95c93be323b443a4c2a5fec3
Nasser <nasser.mahamat.swe@gmail.com>
2026-04-13 20:40:20 +0300

    add Metric class with fields, constroctor, getters/setters, and calculateScore() method

--- src/model/Metric.java
+++ src/model/Metric.java
@@ -1,4 +1,62 @@
 package model;
 
 public class Metric {
+
+    // Private fields
+    private String name;
+    private int weight;
+    private String direction;
+    private double minValue;
+    private double maxValue;
+    private String unit;
+
+    private double measuredValue;
+
+    // Constructor
+    Metric(String name, int weight, String direction, double minValue, double maxValue, String unit) {
+        this.name = name;
+        this.weight = weight;
+        this.direction = direction;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+        this.unit = unit;
+    }
+
+    // getters
+    public String getName() { return name; }
+    public int getWeight() { return weight; }
+    public String getDirection() { return direction; }
+    public double getMinValue() { return minValue; }
+    public double getMaxValue() { return maxValue; }
+    public String getUnit() { return unit; }
+
+    // setters
+    public void setMeasuredValue() {
+        this.measuredValue = measuredValue;
+    }
+
+    // Methods
+    public double calculateScore() {
+        double score;
+
+        // Calculate raw score
+        if (direction.equals("higher")) {
+            score = 1 + (measuredValue - minValue) / (maxValue - minValue) * 4;
+        }
+        else {
+            score = 5 - (measuredValue - minValue) / (maxValue - minValue) * 4;
+        }
+
+        // Clamp between 1 and 5
+        if (score < 1)
+            score = 1.00;
+
+        if (score > 5)
+            score = 5.00;
+
+        // Round to mearest 0.5
+        score = Math.round(score * 2) / 2.0;
+
+        return score;
+    }
 }
