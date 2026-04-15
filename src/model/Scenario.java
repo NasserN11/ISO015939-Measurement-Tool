b5c6816bf2d002a6589e44151dc718e8770b64f1
Nasser <nasser.mahamat.swe@gmail.com>
2026-04-15 19:34:20 +0300

    Add Scenario class - Constructor initializes name - addDimension() adds dimensions to scenario - claculateOverallScore() calculates overall score of a scenario - findWeakestDimension() fids the dimension with the lowest score - overallLabel() returns label based on score range

--- src/model/Scenario.java
+++ src/model/Scenario.java
@@ -1,4 +1,75 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Scenario {
+
+    // Private fields
+    private String name;
+
+    ArrayList<Dimension> dimensions;
+
+    // Constructor
+    public Scenario(String name) {
+
+        this.name = name;
+        dimensions = new ArrayList<>();
+    }
+
+    // Getters
+    public String getName() { return name; }
+    public ArrayList<Dimension> getDimensions() { return this.dimensions; }
+
+
+    // Methods
+    public void addDimension(Dimension d) { dimensions.add(d); }
+
+    public double calculateOverallScore() {
+
+        // dimensionScore x dimensionWeight
+        double totalScoreTimesWeight = 0;
+        double totalWeight = 0;
+
+        int size = dimensions.size();
+        for (int i = 0; i < size; i++) {
+
+            Dimension d = dimensions.get(i);
+            double dimensionScore = d.calculateDimensionScore();
+
+            totalScoreTimesWeight += dimensionScore * d.getWeight();
+            totalWeight += d.getWeight();
+        }
+
+        double overallScore = totalScoreTimesWeight / totalWeight;
+        overallScore = Math.round(overallScore * 10) / 10.0;
+
+        return overallScore;
+    }
+
+    public Dimension findWeakestDimension() {
+
+        Dimension weakestDimension = dimensions.get(0);
+        double weakestDimensionScore = weakestDimension.calculateDimensionScore();
+
+        int size = dimensions.size();
+        for (int i = 1; i < size; i++) {
+            Dimension dimension = dimensions.get(i);
+            double dimensionScore = dimension.calculateDimensionScore();
+
+            if (dimensionScore < weakestDimensionScore) {
+                weakestDimension = dimension;
+            }
+        }
+
+        return weakestDimension;
+    }
+
+    public String overallLabel() {
+
+        double score = calculateOverallScore();
+        if (score >= 4.5) return "Excellent Quality";
+        if (score >= 3.5) return "Good Quality";
+        if (score >= 2.5) return "Needs Improvement";
+        return "Poor Quality";
+    }
 }
