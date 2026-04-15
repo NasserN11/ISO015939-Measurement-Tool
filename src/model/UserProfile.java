b3d6a22cd6744928de0e09a44047da89250c77ee
Nasser <nasser.mahamat.swe@gmail.com>
2026-04-15 19:49:20 +0300

    Add UserProfile class for storing user info

--- src/model/UserProfile.java
+++ src/model/UserProfile.java
@@ -1,4 +1,21 @@
 package model;
 
 public class UserProfile {
+
+    // Private fields
+    private String username;
+    private String school;
+    private String sessionName;
+
+    // Constructor
+    public UserProfile(String username, String school, String sessionName) {
+        this.username = username;
+        this.school = school;
+        this.sessionName = sessionName;
+    }
+
+    // Getters
+    public String getUsername() { return username; }
+    public String getSchool() { return school; }
+    public String getSessionName() { return sessionName; }
 }
