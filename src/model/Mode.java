34d63cf3df769943060bfcb3c181b2e7d2c9abf2
Nasser <nasser.mahamat.swe@gmail.com>
2026-04-15 19:42:20 +0300

    Add Mode class with addScenario() method

--- src/model/Mode.java
+++ src/model/Mode.java
@@ -1,4 +1,23 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Mode {
+
+    // Private fields
+    private String name;
+    ArrayList<Scenario> scenarios;
+
+    // Constructor
+    public Mode(String name) {
+        this.name = name;
+        scenarios = new ArrayList<>();
+    }
+
+    // Getters
+    public String getName() { return name; }
+    public ArrayList<Scenario> getScenarios() { return this.scenarios; }
+
+    // Methods
+    public void addScenario(Scenario s) { scenarios.add(s); }
 }
